9437786dd7f9cba54548f9731497909e515272d1
yeonjoo.lee <teddyjune@naver.com>
2023-04-17 18:48:03 +0900

    feat: From Json

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> WebtoonInstances = [];
     final url = Uri.parse("$baseUrl/$today");
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        WebtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
     }
-    throw Error();
+    return WebtoonInstances;
   }
 }
